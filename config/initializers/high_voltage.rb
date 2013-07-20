be319c24d199940293704bd3fdd62db323725274
Peter Kordel <pkordel@gmail.com>
2013-07-21 00:15:33 +0200

    Initial setup with simple_form and bootstrap

---
+++ config/initializers/high_voltage.rb
@@ -0,0 +1,3 @@
+# To serve static pages from http://www.example.com/about instead of
+# http://www.example.com/pages/about, uncomment line below
+HighVoltage.route_drawer = HighVoltage::RouteDrawers::Root
